495cc41e5aa534019d88318b9e0981d16d1e5892
sid-thakur47 <sidthakur258@gmail.com>
2020-04-23 17:11:26 +0530

    ADDED:Functionality to handle NullPointerException

--- MoodAnalyzer/src/main/java/com/bl/MoodAnalyzer.java
+++ MoodAnalyzer/src/main/java/com/bl/MoodAnalyzer.java
@@ -1,33 +1,33 @@
 package com.bl;
 
+import java.io.IOException;
+
 public class MoodAnalyzer {
 
     String message;
 
-    public MoodAnalyzer() {
+    public MoodAnalyzer()  {
+         this.message="HAPPY";
     }
-
     public MoodAnalyzer(String message) {
         this.message = message;
     }
     public String getMessage() {
         return message;
     }
-    public String setMessage(String message) {
-        this.message = message;
-        return message;
-    }
 
-    public String analyzeMood() {
-
-        if (message == null) {
-            return "HAPPY";
-        } else if (message.equals( "im in sad mood" )) {
-            return "SAD";
-        } else if (message.equals( "im in happy mood" )) {
-            return "SAD";
-        } else {
+    public String analyzeMood(String message) {
+        try {
+            if (message.equals( "im in happy mood" )) {
+                return "HAPPY";
+            } else {
+                return "SAD";
+            }
+        } catch (NullPointerException e) {
             return "HAPPY";
         }
+    }
+    public String analyzeMood() {
+        return analyzeMood( this.message );
     }
 }
