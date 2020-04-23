23168924bbd07d8d028f3517f8b599f37ef49324
sid-thakur47 <sidthakur258@gmail.com>
2020-04-23 11:11:42 +0530

    DELETED: To clean up code

--- MoodAnalyzer/src/test/java/com/bl/MoodAnalyzerTest.java
+++ MoodAnalyzer/src/test/java/com/bl/MoodAnalyzerTest.java
@@ -8,28 +8,24 @@ public class MoodAnalyzerTest {
     public void given_Message_IamInSadMood_ShouldReturnSad() {
         MoodAnalyzer moodAnalyzer = new MoodAnalyzer( "im in sad mood" );
         String mood = moodAnalyzer.analyzeMood();
-        System.out.println( "sad mood:" + mood );
         Assert.assertEquals( "SAD", mood );
     }
     /*@Test
         public void Not_given_MessageShouldReturnSad() {
             MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
             String mood = moodAnalyzer.analyzeMood();
-            System.out.println("sad mood:"+mood);
-            Assert.assertEquals( "SAD", mood );
+                        Assert.assertEquals( "SAD", mood );
         }*/
     @Test
     public void given_Message_IamInHappyMood_ShouldReturnHappy() {
         MoodAnalyzer moodAnalyzer = new MoodAnalyzer( "im in happy mood" );
         String mood = moodAnalyzer.analyzeMood();
-        System.out.println( "Happy mood:" + mood );
         Assert.assertEquals( "SAD", mood );
     }
     @Test
     public void not_Given_Message_ShouldReturnHappy() {
         MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
         String mood = moodAnalyzer.analyzeMood();
-        System.out.println( "Mood is:" + mood );
         Assert.assertEquals( "HAPPY", mood );
     }
 }
